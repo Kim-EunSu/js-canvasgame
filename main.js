462bdec9362925ebf8f1d863b6a96406b9ef73cd
Kim-EunSu <kes73640@gmail.com>
2023-03-14 11:06:02 +0900

    총알 생성

--- main.js
+++ main.js
@@ -13,6 +13,38 @@ let backgroundImage, bulletImage, enemyIamge, rocketImage, gameoverImage;
 let rocketX = canvas.width / 2 - 48;
 let rocketY = canvas.height - 100;
 
+let bulletList = [];
+function Bullet() {
+  this.x = 0;
+  this.y = 0;
+
+  this.init = function () {
+    this.x = rocketX + 19;
+    this.y = rocketY;
+
+    bulletList.push(this);
+  };
+
+  this.update = function () {
+    this.y -= 7;
+  };
+}
+
+// let bulletList = []; //총알들을 저장하는 리스트
+// function Bullet() {
+//   this.x = 0;
+//   this.y = 0;
+//   this.init = function () {
+//     this.x = rocketX + 19; //총알이 정가운데에서 나오게 하기 위해서 +19를 더해줌
+//     this.y = rocketY;
+
+//     bulletList.push(this);
+//   };
+//   this.update = function () {
+//     this.y -= 7;
+//   };
+//}
+
 //이미지를 불러오는 함수
 function loadImage() {
   backgroundImage = new Image();
@@ -31,53 +63,66 @@ function loadImage() {
   gameoverImage.src = "images/gameover.png";
 }
 
-let keysDown = {};
+let keysdown = {};
 function setupKeyboardListener() {
   document.addEventListener("keydown", function (e) {
-    keysDown[e.keyCode] = true;
-    console.log(keysDown);
+    keysdown[e.keyCode] = true;
+    console.log(keysdown);
   });
   document.addEventListener("keyup", function (e) {
-    delete keysDown[e.keyCode];
-    console.log(keysDown);
+    delete keysdown[e.keyCode];
+    console.log(keysdown);
+
+    //스페이스는 keyCode가 32
+    if (e.keyCode === 32) {
+      createBullet(); //총알 생성
+    }
   });
 }
 
-function update() {
-  if (39 in keysDown) {
+function createBullet() {
+  console.log("총알 나감");
+  let b = new Bullet(); //총알 하나 생성
+  b.init();
+  console.log("총알나감");
+}
+
+function upDate() {
+  if (39 in keysdown) {
     rocketX += 5;
   }
-  if (37 in keysDown) {
+  if (37 in keysdown) {
     rocketX -= 5;
   }
 
   if (rocketX <= 0) {
     rocketX = 0;
   }
+
   if (rocketX >= canvas.width - 96) {
     rocketX = canvas.width - 96;
   }
 
-  // if (38 in keysDown) {
-  //   rocketY -= 5;
-  // }
-  // if (40 in keysDown) {
-  //   rocketY += 5;
-  // }
-
-  //우주선의 좌표값 canvase안에서만 설정
+  //총알의 y좌표 업데이트함수 호출
+  for (let i = 0; i < bulletList.length; i++) {
+    bulletList[i].update();
+  }
 }
 
 //이미지를 보여주는 함수
 function render() {
   ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(rocketImage, rocketX, rocketY);
+
+  for (let i = 0; i < bulletList.length; i++) {
+    ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y);
+  }
 }
 
 function main() {
-  update(); //좌표값을 업데이트하고
-  render(); //그려주고!!!!
-  requestAnimationFrame(main); // 계속 보여줌
+  upDate();
+  render();
+  requestAnimationFrame(main);
 }
 
 loadImage();
