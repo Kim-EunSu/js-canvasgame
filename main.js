bfdee77f370368714c44c4b3d43e2cc0e71fc556
Kim-EunSu <kes73640@gmail.com>
2023-03-14 12:19:48 +0900

    적군 생성

--- main.js
+++ main.js
@@ -9,17 +9,20 @@ document.body.appendChild(canvas);
 
 let backgroundImage, bulletImage, enemyIamge, rocketImage, gameoverImage;
 
+let gameOver = false; //true이면 게임오버, flase이면 게임끝나지 않음
+
 //우주선 좌표
 let rocketX = canvas.width / 2 - 48;
 let rocketY = canvas.height - 100;
 
+//총알
 let bulletList = [];
 function Bullet() {
   this.x = 0;
   this.y = 0;
 
   this.init = function () {
-    this.x = rocketX + 19;
+    this.x = rocketX + 19; //총알이 정가운데 나오게하기 위해서
     this.y = rocketY;
 
     bulletList.push(this);
@@ -30,20 +33,33 @@ function Bullet() {
   };
 }
 
-// let bulletList = []; //총알들을 저장하는 리스트
-// function Bullet() {
-//   this.x = 0;
-//   this.y = 0;
-//   this.init = function () {
-//     this.x = rocketX + 19; //총알이 정가운데에서 나오게 하기 위해서 +19를 더해줌
-//     this.y = rocketY;
+//적군
+function newRandomValue(min, max) {
+  let randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNum;
+}
 
-//     bulletList.push(this);
-//   };
-//   this.update = function () {
-//     this.y -= 7;
-//   };
-//}
+let enemyList = [];
+function Enemy() {
+  this.x = 0;
+  this.y = 0;
+
+  this.init = function () {
+    this.x = newRandomValue(0, canvas.width - 96); //랜덤으로 나와야하기 때문에 새로운 함수생성
+    this.y = 0; //최상단에서 출발
+
+    enemyList.push(this);
+  };
+
+  this.update = function () {
+    this.y += 3; //적군의 속도조절
+
+    if (this.y >= canvas.height - 48) {
+      gameOver = true;
+      console.log("gameover");
+    }
+  };
+}
 
 //이미지를 불러오는 함수
 function loadImage() {
@@ -57,7 +73,7 @@ function loadImage() {
   bulletImage.src = "images/bullet.png";
 
   enemyIamge = new Image();
-  enemyIamge.src = "image/enemy.png";
+  enemyIamge.src = "images/enemy.png";
 
   gameoverImage = new Image();
   gameoverImage.src = "images/gameover.png";
@@ -87,6 +103,15 @@ function createBullet() {
   console.log("총알나감");
 }
 
+function createEnemy() {
+  //1초마다 생성
+  // const interval = setInterval(호출하고싶은 함수, 시간);
+  const interval = setInterval(function () {
+    let e = new Enemy(); //적군 생성
+    e.init();
+  }, 1000);
+}
+
 function upDate() {
   if (39 in keysdown) {
     rocketX += 5;
@@ -107,6 +132,10 @@ function upDate() {
   for (let i = 0; i < bulletList.length; i++) {
     bulletList[i].update();
   }
+
+  for (let i = 0; i < enemyList.length; i++) {
+    enemyList[i].update();
+  }
 }
 
 //이미지를 보여주는 함수
@@ -117,14 +146,23 @@ function render() {
   for (let i = 0; i < bulletList.length; i++) {
     ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y);
   }
+
+  for (let i = 0; i < enemyList.length; i++) {
+    ctx.drawImage(enemyIamge, enemyList[i].x, enemyList[i].y);
+  }
 }
 
 function main() {
-  upDate();
-  render();
-  requestAnimationFrame(main);
+  if (!gameOver) {
+    upDate();
+    render();
+    requestAnimationFrame(main);
+  } else {
+    ctx.drawImage(gameoverImage, 0, 100, 400, 300);
+  }
 }
 
 loadImage();
 setupKeyboardListener();
+createEnemy(); //웹페이지 시작하자마자 적군들도 내려옴
 main();
